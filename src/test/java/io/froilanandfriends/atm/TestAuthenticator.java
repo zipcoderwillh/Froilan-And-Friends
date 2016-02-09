37ba8e376da3b3a805533d488bb0750ce723f674
Joe Roddy <mrjoeroddy@gmail.com>
2016-02-09 14:10:13 -0500

    add authenticator test

---
+++ src/test/java/io/froilanandfriends/atm/TestAuthenticator.java
@@ -0,0 +1,33 @@
+package io.froilanandfriends.atm;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TestAuthenticator {
+    UserManager useM = new UserManager();
+    useM.addUser("jbutts12","J","R","j@gmail.com",1111,"applesauce?","yes");
+    Authenticator auth = Authenticator.getAuthenticator();
+    @Test
+    public void testValidateUser(){
+        User testUser = auth.validateUser("kbutts12");
+        assertNull("validateUser() test failed",testUser);
+
+        testUser = auth.validateUser("jbutts12");
+        assertNotNull("validateUser() test failed",testUser);
+        assertTrue("validateUser() test failed",testUser.getEmail().equals("j@gmail.com"));
+
+        System.out.println("All validateUser() tests passed.");
+    }
+
+    @Test
+    public void testAuthenticate(){
+        boolean answer = auth.authenticate("jbutts12",1110);
+        assertTrue("authenticate() test failed",answer=false);
+        answer = auth.authenticate("jbutts2",1111);
+        assertTrue("authenticate() test failed",answer=false);
+        answer = auth.authenticate("jbutts12",1111);
+        assertTrue("authenticate() test failed",answer=true);
+
+        System.out.println("All authenticate() tests passed.");
+    }
+}
