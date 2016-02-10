fd0e4b9c200db01fec60d754a2c457330eac9342
Francisco Castellanos <francisco@docdevelopers.com>
2016-02-10 14:08:29 -0500

    Tests

--- src/test/java/io/froilanandfriends/atm/TransactionManagerTest.java
+++ src/test/java/io/froilanandfriends/atm/TransactionManagerTest.java
@@ -2,6 +2,9 @@ package io.froilanandfriends.atm;
 
 import org.junit.Test;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -42,6 +45,44 @@ public class TransactionManagerTest {
 
     @Test
     public void testLoadTransactions() throws Exception {
+        /****
+         loadTransactions looks in the file transactionsLog.csv
+         and makes a transaction object with each line of the file, and stores each in
+         manager's transactions array
+         The test runs successfully when the to-account # we read from line 1 is the same
+         as the to-account of transaction object at index 0 if the array. There is something a bit
+         wonky right now because one of the factory's in transactionManager makes the accounts in an order
+         different than normal.
+         ****/
+        TransactionManager manager = TransactionManager.getTransactionManager();
+        manager.loadTransactions();
+        Transaction transaction = manager.getAllTransactions().get(0);
+        assertEquals(1899888, transaction.getToAccount());
+
+    }
+
+    @Test
+    public void testLogTransactions() throws Exception {
+        /** test shows that after we stock manager's transaction array, logTransactions()
+         * can take those objects create a long string with them and write it to
+         * a file. The test shows the file exists and we can see the contents are in order
+         * when we print out
+         **/
+        TransactionManager manager = TransactionManager.getTransactionManager();
+        //                                        type      to      from    amount
+        manager.createTransaction(TransactionType.DEPOSIT, 1899888, 1888181, 80000);
+        manager.createTransaction(TransactionType.DEPOSIT, 1899888, 1888181, 80000);
+        manager.createTransaction(TransactionType.DEPOSIT, 1899888, 1888181, 80000);
+        manager.logTransactions();
+
+        File file = new File("transactionsLog.csv");
+
+        assertTrue("File does not exist :)",file.exists());
+        BufferedReader br = new BufferedReader(new FileReader(file));
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
 
     }
 
