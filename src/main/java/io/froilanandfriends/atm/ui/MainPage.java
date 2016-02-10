0e25a30de3228132477f3185131e43a20b2790f9
Francisco Castellanos <francisco@docdevelopers.com>
2016-02-10 13:49:16 -0500

    UI stuff

--- src/main/java/io/froilanandfriends/atm/ui/MainPage.java
+++ src/main/java/io/froilanandfriends/atm/ui/MainPage.java
@@ -1,7 +1,38 @@
 package io.froilanandfriends.atm.ui;
 
+import javafx.application.Application;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 /**
  * Created by Doc on 2/10/16.
  */
-public class MainPage {
+public class MainPage  extends Application {
+    Stage window;
+    Button button;
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        window = primaryStage;
+        window.setTitle("Froilan and Friends ATM");
+        button = new Button("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 250);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+
+
+
 }
