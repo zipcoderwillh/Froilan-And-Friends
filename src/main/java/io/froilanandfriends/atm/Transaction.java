febf6418f51dda5f1c07de53dd5c0b0b162f9a0a
Shaun Gould <shaungould@gmail.com>
2016-02-11 14:12:16 -0500

    more comments

--- src/main/java/io/froilanandfriends/atm/Transaction.java
+++ src/main/java/io/froilanandfriends/atm/Transaction.java
@@ -2,14 +2,29 @@ package io.froilanandfriends.atm;
 
 import java.util.Date;
 
+/**
+ * <h1>Class Transaction</h1>
+ */
+
+
 public class Transaction {
-    //dateTime
+
+    /**
+     * public class for transaction objects. Any interaction involving money at the ATM
+     * will generate an instance of this class
+     */
+
+
+
+
+
     private Date date;
     private long fromAccount,toAccount;
     private double amount;
     private long id;
     //private static long transactionIdCount = 0;
     TransactionType transactionType;
+
 
 
     /*** This is the constructor we will use to load transaction objects from history***/
@@ -41,30 +56,63 @@ public class Transaction {
         this(mType, mFromAccount, -1, mAmount, mId);
     }
 
+    /**
+     * <h1>Method Summary</h1>
+     */
+
+    /**
+     *
+     * @return Date, date object of the instance
+     */
     public Date getDate() {
         return date;
     }
 
+    /**
+     *
+     * @return long, fromAccount of transaction
+     */
     public long getFromAccount() {
         return fromAccount;
     }
 
+    /**
+     *
+     * @return long, toAccount of transaction
+     */
     public long getToAccount() {
         return toAccount;
     }
 
+    /**
+     *
+     * @return double, amount of transaction
+     */
     public double getAmount() {
         return amount;
     }
 
+    /**
+     *
+     * @return long, id of transaction
+     */
     public long getId() {
         return id;
     }
 
+    /**
+     *
+     * @return Transaction Type, enum representing transactionType
+     */
     public TransactionType getTransactionType() {
         return transactionType;
     }
 
+    /**
+     *
+     * @param date a Date object
+     * @return void
+     */
     public void setDate(Date date) {
         this.date = date;
     }
