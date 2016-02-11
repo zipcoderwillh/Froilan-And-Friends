330d96a962cb1878610fe1ac32e0478f623950d3
willhorton <willhorton@Zips-MacBook-Pro-4.local>
2016-02-11 14:51:59 -0500

    Added annotations to ATM, Autheticator, UserMenu, and LoginMenu

--- src/main/java/io/froilanandfriends/atm/ATM.java
+++ src/main/java/io/froilanandfriends/atm/ATM.java
@@ -2,6 +2,16 @@ package io.froilanandfriends.atm;
 
 import java.util.HashMap;
 
+/**
+ * This class houses the core ATM functionality, including withdrawals, deposits, and ensuring that the ATM's
+ * maximum capacity is not exceeded. It also handles admin user functions such as emptying and refilling
+ * the withdrawal and deposit trays. The class is implemented as a singleton in order to avoid creating more than
+ * one instance at a time.
+ *
+ * @author willhorton
+ * @version 1.0
+ */
+
 public class ATM {
 
     private int depositTray; // Total number of bills deposited in the machine
@@ -9,9 +19,9 @@ public class ATM {
     private HashMap<Integer,Integer> withdrawalTray = new HashMap<Integer, Integer>();  // Number of 20-dollar and 10-dollar bills
     private int ATMBalance;  // Total dollar amount available to WITHDRAW (does not include value of deposits
     private final int MAXCAPACITY = 2000;   // Max number of bills the ATM can hold, including deposit and
+                                            // withdrawal trays. (Arbitrarily set to 2000 for now.)
+    private static boolean firstOn = true;
 
-    public static boolean firstOn = true;
-    // withdrawal trays. (Arbitrarily set to 2000 for now.)
 
     // Make constructor private so nothing can instantiate ATM outside of singleton instance
     private ATM() {
@@ -25,10 +35,39 @@ public class ATM {
     // Set up singleton instance
     private static final ATM CURRENT = new ATM();
 
+    /**
+     * Getter method that returns the current singleton instance of the class.
+     * @return A reference to current singleton ATM instance.
+     */
     public static ATM getATM() {
         return CURRENT;
     }
 
+    /**
+     * Returns true if the ATM is booting up for the first time.
+     * @return True or false.
+     */
+    public static boolean isFirstOn() {
+        return firstOn;
+    }
+
+    /**
+     * Allows admin to set the firstOn flag to false after ATM is configured initially.
+     * @param firstOn A boolean flag indicating whether the ATM is booting up for the first time.
+     */
+    public static void setFirstOn(boolean firstOn) {
+        ATM.firstOn = firstOn;
+    }
+
+    /**
+     * Withdraws cash from the ATM in increments of ten- or twenty-dollar bills. Rejects the transaction
+     * if the requested amount is not divisible by ten or if the withdrawal amount exceeds the current value of the
+     * ATM's withdrawal tray. If the transaction is successful, decreases both the ATM's current balance and the
+     * number of bills in the machine. Covers as much of the amount as possible with twenties, then covers the rest
+     * with tens.
+     * @param amountToWithdraw An integer specifying the requested withdrawal amount.
+     * @return True if deposit is successful, false if unsuccessful.
+     */
     public boolean withdraw(int amountToWithdraw){
 
         // First, check if there is enough money in the ATM to cover the withdrawal at all
@@ -73,6 +112,14 @@ public class ATM {
         return result;
     }
 
+    /**
+     * Provides admin functionality to reload the withdrawal tray with any number of tens and/or twenties. Rejects
+     * the reload if the amount would exceed the ATM's total bill capacity. If successful, increases both the ATM's
+     * current balance and the number of bills in the withdrawal tray.
+     * @param twenties An integer specifiying the number of twenties to be loaded.
+     * @param tens An integer specifiying the number of tens to be loaded.
+     * @return True if transaction was successful, false if it was not.
+     */
     public boolean reloadWithdrawalTray(int twenties, int tens) {
 
         boolean result = false;
@@ -91,6 +138,14 @@ public class ATM {
 
     }
 
+    /**
+     * Allows customers to deposit any whole number amount, provided that the amount does not exceed the ATM's
+     * total bill capacity. If successful, increases both the ATM's current balance and the number of bills in the
+     * deposit tray. Assumes that the amount consists of the greatest number of twenties possible, with the rest
+     * covered by a combination of tens, fives, and ones.
+     * @param amount An integer specifying the amount to deposit (the ATM will only accept deposits in bills).
+     * @return True if transaction is successful, false if unsuccessful.
+     */
     public boolean deposit(int amount){
 
         boolean result = false;
@@ -124,30 +179,50 @@ public class ATM {
         return result;
     }
 
-    // Admin functionality
+    /**
+     * Provides admin functionality to empty the ATM's deposit tray.
+     */
     public void emptyDepositTray(){
         depositTray = 0;
         depositValue = 0;
     }
 
+    /**
+     * Returns the current number of bills in the ATM's deposit tray.
+     * @return An integer specifying the number of bills in the deposity tray.
+     */
     public int getDepositTray() {
         return depositTray;
     }
 
+    /**
+     * Returns the current dollar value of the ATM's deposit tray.
+     * @return An integer specifying the value of the deposit tray.
+     */
     public int getDepositValue() {
         return depositValue;
     }
 
+    /**
+     * Returns the current number of bills in the ATM's withdrawal tray.
+     * @return A HashMap with keys 20 and 10 that track the total number of twenties and tens in the ATM's withdrawal tray.
+     */
     public HashMap<Integer, Integer> getWithdrawlTray() {
         return withdrawalTray;
     }
 
+    /**
+     * Returns the current dollar value of the ATM's withdrawal tray. Does <b>NOT</b> include the value of the deposit tray.
+     * @return An integer specifying the dollar value of the withdrawal tray.
+     */
     public int getATMBalance() {
         return ATMBalance;
     }
 
-    public static void main(String[] args) {
-
+    /**
+     * Initializes UserManager
+     */
+    public void start() {
         try {
             UserManager.getUserManager().loadUsers();
         } catch(Exception e) {
@@ -164,14 +239,12 @@ public class ATM {
         }
 
         LoginMenu.promptCredentials();
-
-    }
-    public static boolean isFirstOn() {
-        return firstOn;
     }
 
-    public static void setFirstOn(boolean firstOn) {
-        ATM.firstOn = firstOn;
+    public static void main(String[] args) {
+
+        ATM.getATM().start();
+
     }
 
 }
