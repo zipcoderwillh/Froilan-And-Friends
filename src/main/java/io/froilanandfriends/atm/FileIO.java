a0216c315d01472869834b3ce4ba697a8e015056
Shaun Gould <shaungould@gmail.com>
2016-02-11 12:28:15 -0500

    comments for javadoc

--- src/main/java/io/froilanandfriends/atm/FileIO.java
+++ src/main/java/io/froilanandfriends/atm/FileIO.java
@@ -7,11 +7,27 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.lang.StringBuilder;
 
+/**
+ * <h>READING AND WRITING WITH PERSISTENT FILES</h>
+ * This class is used by all the manager classes to either
+ * read or log their data.
+ */
+
+
+
+
 public class FileIO {
 
-    /*** FILE READING - Reads file by line, rebuilds as String, sends off ***/
-    // This is the new way to do this.  Each MANAGER that needs to set records should now have
-    // a String pathname = its log file path.  When it calls setRecords, it should pass its pathname.
+    /**
+     * <h>FILE READING</h>
+     *
+     * Reads file specified by arg {@Code String path} by line, and builds
+     * one big String from the contents of that file.
+     * <p>
+     * Called within load methods in each of the Managers.
+     * Each MANAGER that needs to read or write records should now have
+     * a String pathname for the file it wants to use.
+     */
     public static String readRecords(String path) throws Exception {
         String line;
         BufferedReader br = new BufferedReader(new FileReader(path));
@@ -22,9 +38,14 @@ public class FileIO {
         br.close();
         return stringBuilder.toString().trim();
     }
-    /****  END OF FILE READING  ****/
 
-    /*** FILE WRITING -  Receives long string from Manager, writes it to file - x is long String, y is file path ***/
+
+    /**
+     *  <h>FILE WRITING</h>
+     *  Called by the log methods of each Manager class.
+     *  Receives long string from said methods, writes it to file - x is long String, y is file path,
+     *  usually specified by the PATHNAME field in each Manager.
+     */
     // This is the new way to do this.  Each MANAGER that needs to set records should now have
     // a String pathname = its log file path.  When it calls setRecords, it should pass its pathname.
     public static void logRecords(String outString, String path) throws Exception {
@@ -32,5 +53,5 @@ public class FileIO {
         out.write(outString.trim());
         out.close();
     }
-    /**** END OF FILE WRITING  ****/
+
 }
