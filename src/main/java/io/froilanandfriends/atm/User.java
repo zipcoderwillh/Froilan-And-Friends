2b4ea31cb22ec289dcb7a5cb66a9dc02ba6358a3
willhorton <willhorton@Zips-MacBook-Pro-4.local>
2016-02-11 15:43:42 -0500

    Added annotations to User and UserManager

--- src/main/java/io/froilanandfriends/atm/User.java
+++ src/main/java/io/froilanandfriends/atm/User.java
@@ -1,6 +1,9 @@
 package io.froilanandfriends.atm;
 
-
+/**
+ * This class houses all variables and methods related to individual users, including username, security questions,
+ * flags for admin access or fraudulent activity, etc.
+ */
 
 public class User {
     private boolean isAdmin = false;
@@ -9,6 +12,11 @@ public class User {
     private int userID = um.getAllUsers().size(),pin;
     private String userName, firstName, lastName, email, securityQuestion, securityAnswer;
 
+    /**
+     * Authenticates a PIN number entered by a customer against the PIN of an existing User.
+     * @param pin The PIN number passed to the method.
+     * @return True if PIN number matches, false if it does not.
+     */
     boolean authenticate(int pin){
         if(pin==this.pin){
             return true;
@@ -16,7 +24,10 @@ public class User {
         return false;
     }
     /*** FROM FILE IO  ***/
-    //Added general constructor for loading purposes. String x comes from UserManager.loadUsers().
+    /**
+     * Constructor for loading an existing user from persistent storage on ATM bootup.
+     * @param x Passed from {@link UserManager.loadUsers()}.
+     */
     public User(String x){
         String[] cSeparated = x.split(",");
         this.userName  = cSeparated[0];
@@ -32,6 +43,16 @@ public class User {
     }
     /*** END OF FILE IO ***/
 
+    /**
+     * Constructor for creating an entirely new user.
+     * @param userName Entered by user during account creation process.
+     * @param firstName Entered by user during account creation process.
+     * @param lastName Entered by user during account creation process.
+     * @param email Entered by user during account creation process.
+     * @param pin Entered by user during account creation process.
+     * @param securityQuestion Entered by user during account creation process.
+     * @param securityAnswer Entered by user during account creation process.
+     */
     public User(String userName, String firstName, String lastName, String email, int pin, String securityQuestion, String securityAnswer){
         this.userName = userName;
         this.firstName = firstName;
@@ -43,64 +64,126 @@ public class User {
         this.userID += 42;
     }
 
+    /**
+     * Sets fraudulent activity flag to true.
+     */
     public void setFlagged(){
         flagged = true;
     }
 
+    /**
+     * Removes fraudulent activity flag.
+     */
     public void removeFlagged(){
         flagged = false;
     }
+
+    /**
+     * Sets PIN number.
+     * @param pin
+     */
     public void setPin(int pin) {
         this.pin = pin;
     }
 
+    /**
+     * Accessor method for getting PIN number.
+     * @return
+     */
     public int getPin(){
         return this.pin;
     }
+
+    /**
+     * Flags user as having admin access.
+     */
     public void setAsAdmin(){
         isAdmin = true;
     }
 
+    /**
+     * Removes admin flag.
+     */
     public void removeAdminStatus(){
         isAdmin = false;
     }
 
+    /**
+     * Returns flag indicating whether user has admin access.
+     * @return True or false.
+     */
     public boolean isAdmin() {
         return isAdmin;
     }
 
+    /**
+     * Shows whether user has been flagged for fraudulent activity.
+     * @return True or false.
+     */
     public boolean isFlagged() {
         return flagged;
     }
 
+    /**
+     * Sets the user's ID.
+     * @param ID
+     */
     public void setUserID(int ID){
         this.userID = ID;
     }
 
+    /**
+     * Returns the user's ID.
+     * @return ID.
+     */
     public int getUserID() {
         return userID;
     }
 
+    /**
+     * Returns the user's name.
+     * @return User's name.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Returns the user's first name.
+     * @return User's first name.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Returns the user's last name.
+     * @return User's last name.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Returns the user's email.
+     * @return User's email.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Returns the user's security question.
+     * @return User's security question.
+     */
     public String getSecurityQuestion() {
         return securityQuestion;
     }
 
+    /**
+     * Returns the user's security answer.
+     * @return User's security answer.
+     */
     public String getSecurityAnswer() {
         return securityAnswer;
     }
--- src/main/java/io/froilanandfriends/atm/UserManager.java
+++ src/main/java/io/froilanandfriends/atm/UserManager.java
@@ -2,6 +2,10 @@ package io.froilanandfriends.atm;
 import java.util.ArrayList;
 import java.lang.StringBuilder;
 
+/**
+ * This class manages all users currently in the ATM's database.
+ */
+
 public class UserManager {
     private User currentUser;
     private ArrayList<User> allUsers = new ArrayList<User>();
@@ -10,18 +14,32 @@ public class UserManager {
     //Singleton Setup
     private static UserManager current = new UserManager();
     UserManager(){ }
+
+    /**
+     * Accessor method for the current singleton instance.
+     * @return The current singleton instance of UserManager.
+     */
     public static UserManager getUserManager(){
         return current;
     }
 
     /*** FROM FILEIO ***/
-    // Receives long String from FILEIO, breaks by line, sends each line to Constructor.
+    /**
+     * Receives long string passed from {@link FileIO}, parses by line, and sends each line to constructor. Runs on ATM
+     * bootup to load existing users.
+     * @throws Exception
+     */
     public void loadUsers() throws Exception {
         String[] hrSeparated = FileIO.readRecords(PATHNAME).split("\\n");
         for(String a : hrSeparated){
             allUsers.add(new User(a));
         }
     }
+
+    /**
+     * Outputs each user with all of their attributes to persistent storage.
+     * @throws Exception
+     */
     // For each User in UserArrayList, add User attributes to stringbuilder, send long string to FILEIO.
     public void logUsers () throws Exception {
         StringBuilder stringBuilder = new StringBuilder();
@@ -42,35 +60,70 @@ public class UserManager {
         FileIO.logRecords(stringBuilder.toString(),PATHNAME);
     }
     /*** END OF FILEIO  ***/
+    /**
+     * Resets the current user to null.
+     */
     public void clearUser(){ //removes currentUser
         currentUser = null;
     }
 
+    /**
+     * Sets the path to the external storage file containing all users.
+     * @param PATHNAME Path to external storage.
+     */
     public static void setPATHNAME(String PATHNAME) {
         UserManager.PATHNAME = PATHNAME;
     }
 
+    /**
+     * Flags a user for potential fraudulent activity.
+     * @param userToFlag
+     * @throws Exception
+     */
     public void flagUser(User userToFlag) throws Exception { //flags a user
         userToFlag.setFlagged();
         logUsers();
     }
+
+    /**
+     * Removes fraudulent activity flag from user.
+     * @param UserToUnFlag
+     * @throws Exception
+     */
     public void unFlagUser(User UserToUnFlag) throws Exception{ //removes flag from user
         UserToUnFlag.removeFlagged();
         logUsers();
     }
 
+    /**
+     * Returns the current user.
+     * @return Current user.
+     */
     public User getCurrentUser() {
         return currentUser;
     }
 
+    /**
+     * Sets the current user.
+     * @param currentUser
+     */
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
     }
 
+    /**
+     * Returns an {@link ArrayList} of all Users.
+     * @return All users.
+     */
     public ArrayList<User> getAllUsers() {
         return allUsers;
     }
 
+    /**
+     * Searches for a user by username.
+     * @param userName Username to search for.
+     * @return
+     */
     public User getUser(String userName){ //searches for a user based on userName
         for(int x=0;x<allUsers.size();x++){
             User thisUser = allUsers.get(x);
@@ -81,6 +134,11 @@ public class UserManager {
         return null;
     }
 
+    /**
+     * Searches for user by email.
+     * @param email Email address to search for.
+     * @return
+     */
     public User getUserByEmail(String email){ // searches for a user based on email
         for (int i =0; i<allUsers.size(); i++){
             User thisUser = allUsers.get(i);
@@ -91,20 +149,36 @@ public class UserManager {
         return null;
     }
 
-    //adds a user to the allusers list
+    /**
+     * Adds a user and all their attributes to the allUsers list.
+     * @param userName
+     * @param firstName
+     * @param lastName
+     * @param email
+     * @param pin
+     * @param securityQuestion
+     * @param secruityAnswer
+     * @throws Exception
+     */
     public void addUser(String userName, String firstName, String lastName, String email, int pin, String securityQuestion, String secruityAnswer) throws Exception {
         User newUser = new User(userName, firstName, lastName, email, pin, securityQuestion, secruityAnswer);
         allUsers.add(newUser);
-
-
-
         logUsers();
-
     }
-    //will remove specific user from list
+
+    /**
+     * Removes a specific user from the allUsers list.
+     * @param u User to remove.
+     */
     public void removeCurrentUser(User u){
         allUsers.remove(u);
     }
+
+    /**
+     * Searches for a user by ID.
+     * @param id User ID to search for.
+     * @return
+     */
     public User getUserByID(int id) {
         for (int i =0; i<allUsers.size(); i++){
         User thisUser = allUsers.get(i);
